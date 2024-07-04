74903b51639ec4d0e2b94fb27af94a61091984f2
xiezhaokun <xiezhaokun@huawei.com>
2024-07-04 16:27:23 +0800

    SM3 code cleaning and optimization and adding test cases

--- jca/src/main/java/org/openeuler/SM3.java
+++ jca/src/main/java/org/openeuler/SM3.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (c) 2024, Huawei Technologies Co., Ltd. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,69 +25,57 @@
 package org.openeuler;
 
 import org.openeuler.util.Util;
+
 import java.security.MessageDigestSpi;
 import java.util.Arrays;
 
-public class SM3 extends MessageDigestSpi implements Cloneable{
+public class SM3 extends MessageDigestSpi implements Cloneable {
     private static final int DIGEST_LENGTH = 32;   // Byte length of the final result (256 bits, 32 bytes)
     private static final int BLOCK_BYTE_SIZE = 512 / 8; // Byte length of the message block, (512 bits, 64 bytes, 16 words)
     private long byteCount;
 
-    private final byte[]  byteBuf = new byte[BLOCK_BYTE_SIZE];  // Message block buffer
+    private byte[] byteBuf = new byte[BLOCK_BYTE_SIZE];  // Message block buffer
     private int byteBufOff;
 
-    private final int[] V = new int[8]; // 8 words
-    private final int[] W = new int[68];
-
-    /*
-    * Calculate the Tj value in advance for the compression function
-    *
-    * Tj = 79cc4519        when 0  < = j < = 15
-    * Tj = 7a879d8a        when 16 < = j < = 63
-    * Tj = (Tj <<< (j mod 32))
-    */
-    private static final int[] T = new int[64];
-    static
-    {
-        for (int i = 0; i < 16; ++i)
-        {
-            // T[i] = 0x79CC4519 <<< i
-            T[i] = Util.rotateShiftLeft(0x79CC4519, i);
-        }
-        for (int i = 16; i < 64; ++i)
-        {
-            // T[i] = 0x7A879D8A <<< (i mod 32)
-            T[i] = Util.rotateShiftLeft(0x7A879D8A, i % 32);
-        }
-    }
-
+    private int[] V = new int[8]; // 8 words
+    private int[] W = new int[68];
 
     /**
-    * Standard constructor
-    */
-    public SM3()
-    {
+     * Calculate the Tj value in advance for the compression function
+     *
+     * Tj = 79cc4519        when 0  < = j < = 15
+     * Tj = 7a879d8a        when 16 < = j < = 63
+     * Tj = (Tj <<< (j mod 32))
+     *
+     */
+    private static final int[] ROUND_CONSTS = {
+            0x79CC4519,0xF3988A32,0xE7311465,0xCE6228CB,
+            0x9CC45197,0x3988A32F,0x7311465E,0xE6228CBC,
+            0xCC451979,0x988A32F3,0x311465E7,0x6228CBCE,
+            0xC451979C,0x88A32F39,0x11465E73,0x228CBCE6,
+            0x9D8A7A87,0x3B14F50F,0x7629EA1E,0xEC53D43C,
+            0xD8A7A879,0xB14F50F3,0x629EA1E7,0xC53D43CE,
+            0x8A7A879D,0x14F50F3B,0x29EA1E76,0x53D43CEC,
+            0xA7A879D8,0x4F50F3B1,0x9EA1E762,0x3D43CEC5,
+            0x7A879D8A,0xF50F3B14,0xEA1E7629,0xD43CEC53,
+            0xA879D8A7,0x50F3B14F,0xA1E7629E,0x43CEC53D,
+            0x879D8A7A,0x0F3B14F5,0x1E7629EA,0x3CEC53D4,
+            0x79D8A7A8,0xF3B14F50,0xE7629EA1,0xCEC53D43,
+            0x9D8A7A87,0x3B14F50F,0x7629EA1E,0xEC53D43C,
+            0xD8A7A879,0xB14F50F3,0x629EA1E7,0xC53D43CE,
+            0x8A7A879D,0x14F50F3B,0x29EA1E76,0x53D43CEC,
+            0xA7A879D8,0x4F50F3B1,0x9EA1E762,0x3D43CEC5
+    };
+
+    public SM3() {
         engineReset();
-    }
-
-    /**
-    * Copy constructor.  This will copy the state of the provided
-    * message digest.
-    */
-    public SM3(SM3 t)
-    {
-        System.arraycopy(t.V, 0, this.V, 0, this.V.length);
-        System.arraycopy(t.byteBuf, 0, this.byteBuf, 0, this.byteBuf.length);
-        this.byteCount = t.byteCount;
-        this.byteBufOff = t.byteBufOff;
     }
 
     @Override
     protected void engineUpdate(byte input) {
         byteBuf[byteBufOff++] = input;
 
-        if (byteBufOff == byteBuf.length)
-        {
+        if (byteBufOff == byteBuf.length) {
             ME(); // Message extension
             CF(); // Compression Function
         }
@@ -96,17 +84,14 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
 
     @Override
     protected void engineUpdate(byte[] input, int offset, int len) {
-        len = Math.max(0,  len);
+        len = Math.max(0, len);
 
         // fill the current message group
         int i = 0;
-        if (byteBufOff != 0)
-        {
-            while (i < len)
-            {
+        if (byteBufOff != 0) {
+            while (i < len) {
                 byteBuf[byteBufOff++] = input[offset + i++];
-                if (byteBufOff == byteBuf.length)
-                {
+                if (byteBufOff == byteBuf.length) {
                     ME(); // Message extension
                     CF(); // Compression Function
                     break;
@@ -116,16 +101,14 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
 
         // process whole message block.
         int limit = ((len - i) & ~63) + i;
-        for (; i < limit; i += 64)
-        {
+        for (; i < limit; i += 64) {
             System.arraycopy(input, offset + i, this.byteBuf, 0, 64);
             ME(); // Message extension
             CF(); // Compression Function
         }
 
         // load in the remainder.
-        while (i < len)
-        {
+        while (i < len) {
             byteBuf[byteBufOff++] = input[offset + i++];
         }
 
@@ -137,13 +120,13 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
         long bitLength = (byteCount << 3);
 
         // Add the bit "1" to the end of the message.
-        engineUpdate((byte)128);
+        engineUpdate((byte) 128);
 
         padding(bitLength); // Padding the message
         ME(); // Message extension
         CF(); // Compression Function
 
-        byte[]  digestBytes = new byte[engineGetDigestLength()];
+        byte[] digestBytes = new byte[engineGetDigestLength()];
         Util.intToBigEndian(V, digestBytes, 0);
 
         engineReset();
@@ -153,47 +136,45 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
 
     @Override
     protected void engineReset() {
-         for (int i = 0; i < byteBuf.length; i++)
-         {
-             this.byteBuf[i] = 0;
-         }
+        for (int i = 0; i < byteBuf.length; i++) {
+            this.byteBuf[i] = 0;
+        }
 
-         this.V[0] = 0x7380166F;
-         this.V[1] = 0x4914B2B9;
-         this.V[2] = 0x172442D7;
-         this.V[3] = 0xDA8A0600;
-         this.V[4] = 0xA96F30BC;
-         this.V[5] = 0x163138AA;
-         this.V[6] = 0xE38DEE4D;
-         this.V[7] = 0xB0FB0E4E;
+        this.V[0] = 0x7380166F;
+        this.V[1] = 0x4914B2B9;
+        this.V[2] = 0x172442D7;
+        this.V[3] = 0xDA8A0600;
+        this.V[4] = 0xA96F30BC;
+        this.V[5] = 0x163138AA;
+        this.V[6] = 0xE38DEE4D;
+        this.V[7] = 0xB0FB0E4E;
 
-         this.byteCount = 0;
-         this.byteBufOff = 0;
-     }
+        this.byteCount = 0;
+        this.byteBufOff = 0;
+    }
 
-    public int engineGetDigestLength()
-     {
-         return DIGEST_LENGTH;
-     }
+    public int engineGetDigestLength() {
+        return DIGEST_LENGTH;
+    }
 
     /**
      * Permutation function
+     *
      * @param x input number
      * @return P0(X) = X XOR (X <<<  9) XOR (X <<< 17)
      */
-    private int P0(final int x)
-    {
+    private int P0(final int x) {
         // P0(x) = (x ^ (x <<< 9) ^ (x <<< 17);
         return (x ^ Util.rotateShiftLeft(x, 9) ^ Util.rotateShiftLeft(x, 17));
     }
 
     /**
      * Permutation function
+     *
      * @param x input number
      * @return P1(X) = X XOR (X <<< 15) XOR (X <<< 23)
      */
-    private int P1(final int x)
-    {
+    private int P1(final int x) {
         // P1(x) = (x ^ (x <<< 15) ^ (x <<< 23);
         return (x ^ Util.rotateShiftLeft(x, 15) ^ Util.rotateShiftLeft(x, 23));
     }
@@ -201,7 +182,8 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
     /**
      * Boolean function
      * FFj(X;Y;Z) = X XOR Y XOR Z                       when 0  < = j < = 15
-     *            = (X AND Y) OR (X AND Z) OR (Y AND Z) when 16 < = j < = 63
+     * = (X AND Y) OR (X AND Z) OR (Y AND Z) when 16 < = j < = 63
+     *
      * @param j function index
      * @param x X
      * @param y Y
@@ -219,14 +201,15 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
     /**
      * Boolean function
      * GGj(X;Y;Z) = X XOR Y XOR Z                       when 0  < = j < = 15
-     *            = (X AND Y) OR (NOT X AND Z)          when 16 < = j < = 63
+     * = (X AND Y) OR (NOT X AND Z)          when 16 < = j < = 63
+     *
      * @param j function index
      * @param x X
      * @param y Y
      * @param z Z
      * @return GGj(x, y, z)
      */
-    private int GG(final int j,final int x, final int y, final int z) {
+    private int GG(final int j, final int x, final int y, final int z) {
         if (j <= 15) {
             return (x ^ y ^ z);
         } else {
@@ -238,10 +221,10 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
      * Message Extension
      * W[j] = B[i]                                                                         when 0  < = j < = 15
      * W[j] = P1(W[j-16] XOR W[j-9] XOR (W[j-3] <<< 15)) XOR (W[j-13] <<< 7) XOR W[j-6]    when 16 < = j < = 63
-     *
+     * <p>
      * HINT:
-     *     In order to optimize data storage, W' in the message extension will be
-     *     directly calculated when it is used in the compression function.
+     * In order to optimize data storage, W' in the message extension will be
+     * directly calculated when it is used in the compression function.
      */
     protected void ME() {
         for (int j = 0; j < 16; ++j) {
@@ -261,8 +244,7 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
     /**
      * Compression Function
      */
-    protected void CF()
-    {
+    protected void CF() {
         int A = this.V[0];
         int B = this.V[1];
         int C = this.V[2];
@@ -273,11 +255,10 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
         int H = this.V[7];
 
 
-        for (int j = 0; j < 64; ++j)
-        {
+        for (int j = 0; j < 64; ++j) {
             int A_r12 = Util.rotateShiftLeft(A, 12); // A_r12 = A <<< 12
             // SS1 = ((A <<< 12) + E + (T[j] <<< (j mod 32))) <<< 7, T[j] has been processed in advance.
-            int SS1 = Util.rotateShiftLeft(A_r12 + E + T[j], 7);
+            int SS1 = Util.rotateShiftLeft(A_r12 + E + ROUND_CONSTS[j], 7);
             int SS2 = SS1 ^ A_r12;
             int TT1 = FF(j, A, B, C) + D + SS2 + (W[j] ^ W[j + 4]);
             int TT2 = GG(j, E, F, G) + H + SS1 + W[j];
@@ -305,37 +286,39 @@ public class SM3 extends MessageDigestSpi implements Cloneable{
 
     /**
      * Padding
+     *
      * @param bitLength bit length of message length
      */
-    protected void padding(long bitLength)
-    {
-        if (this.byteBufOff > (BLOCK_BYTE_SIZE - 8))
-        {
-            Arrays.fill(byteBuf, byteBufOff, BLOCK_BYTE_SIZE, (byte)0); // fill with zero
+    protected void padding(long bitLength) {
+        if (this.byteBufOff > (BLOCK_BYTE_SIZE - 8)) {
+            Arrays.fill(byteBuf, byteBufOff, BLOCK_BYTE_SIZE, (byte) 0); // fill with zero
             byteBufOff = BLOCK_BYTE_SIZE;
             ME(); // Message extension
             CF(); // Compression Function
         }
         // Fill with zero words, until reach 2nd to last slot
-        if (this.byteBufOff < (BLOCK_BYTE_SIZE - 8))
-        {
-            Arrays.fill(byteBuf, byteBufOff, BLOCK_BYTE_SIZE - 8, (byte)0);
+        if (this.byteBufOff < (BLOCK_BYTE_SIZE - 8)) {
+            Arrays.fill(byteBuf, byteBufOff, BLOCK_BYTE_SIZE - 8, (byte) 0);
             this.byteBufOff = BLOCK_BYTE_SIZE - 8;
         }
 
         // Store input data length in BITS
         for (int i = 64 - 8; i >= 0; i -= 8) {
-            this.byteBuf[this.byteBufOff++] = (byte)(bitLength >>> i);
+            this.byteBuf[this.byteBufOff++] = (byte) (bitLength >>> i);
         }
     }
 
     /**
      * Clone
+     *
      * @return SM3 class
      * @throws CloneNotSupportedException
      */
     public Object clone() throws CloneNotSupportedException {
-        super.clone();
-        return new SM3(this);
+        SM3 copy = (SM3) super.clone();
+        copy.byteBuf = copy.byteBuf.clone();
+        copy.V = copy.V.clone();
+        copy.W = new int[68];
+        return copy;
     }
 }
